aacb97c9ec496b72e7c66ff6ec75ebe0380eaff6
KromaFr <floriqn.simon@gmail.com>
2026-04-30 22:39:35 +0200

    Fin de TP 1 AngularJS
    Ajout de nouveaux controlleurs
    Pages HTML
    Utilisation de bootstrap etc
    
    Pour le moment assez facile à comprendre, quelques corrections effectués avec Antigravity car je bloquais sur 2 erreurs (Mauvaise syntaxe)

--- js/app.js
+++ js/app.js
@@ -1,12 +1,24 @@
-var app = angular.module("gsbRapports",['ngRoute']);
+var app = angular.module("gsbRapports", ['ngRoute']);
 
-app.config(function($routeProvider){
+app.config(function ($routeProvider) {
     $routeProvider
-            .when('/',{
-                templateUrl :'vues/connexion.html',
-                controller :'connexionController'
-            })
-            .otherwise({redirectTo: '/'});
+        .when('/', {
+            templateUrl: 'vues/connexion.html',
+            controller: 'connexionController'
+        })
+        .when('/accueil', {
+            templateUrl: 'vues/accueil.html',
+            controller: 'accueilController'
+        })
+        .when('/rapports', {
+            templateUrl: 'vues/rapports.html',
+            controller: 'rapportsController'
+        })
+        .when('/medecins', {
+            templateUrl: 'vues/medecins.html',
+            controller: 'medecinsController'
+        })
+        .otherwise({ redirectTo: '/' });
 });
 
 
--- js/controllers/controllers.js
+++ js/controllers/controllers.js
@@ -1,5 +1,27 @@
-app.controller('connexionController',function($scope){
- 
+app.controller('connexionController', function ($scope, $location) {
+    $scope.lblLogin = "Login";
+    $scope.lblMdp = "Mot de passe";
+    $scope.btnValider = "Valider";
+    $scope.lblMessage = "Login ou mot de passe incorrect";
+    $scope.valider = function () {
+        if ($scope.login == "admin" && $scope.mdp == "admin") {
+            $scope.msgErreur = false;
+            $location.path('/accueil');
+        }
+        else
+            $scope.msgErreur = true;
+
+    };
 });
 
+app.controller('accueilController', function ($scope) {
+    $scope.titre = "Gestion des rapports de visite";
+});
 
+app.controller('rapportsController', function ($scope) {
+    $scope.titre = "Gestion des rapports";
+});
+
+app.controller('medecinsController', function ($scope) {
+    $scope.titre = "Gestion des médecins";
+});
